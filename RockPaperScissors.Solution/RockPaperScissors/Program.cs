a74a08c54d8b545be336adce60643f30d5f69033
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2020-03-06 15:28:32 -0800

    Update Program.cs

--- RockPaperScissors.Solution/RockPaperScissors/Program.cs
+++ RockPaperScissors.Solution/RockPaperScissors/Program.cs
@@ -1,54 +1,98 @@
 using System;
-// using System.Collections.Generic;
 using RockPaperScissors.Models;
 
 namespace RockPaperScissors
 {
   public class Program
   {
+    public static Game NewGame = new Game();
+    public static bool GameOver = false;
+    public static int Rounds = 1;
+
     public static void Main()
     {
+      try
+      {
+        Console.WriteLine("Enter Player 1's name:");
+        string name1 = Console.ReadLine();
+        Console.WriteLine("Enter Player 2's name:");
+        string name2 = Console.ReadLine();
+        NewGame.AssignNames(name1, name2);
 
-      Console.WriteLine("Enter Player 1's name:");
-      string name1 = Console.ReadLine();
-      Console.WriteLine("Enter Player 2's name:");
-      string name2 = Console.ReadLine();
-      Game game = new Game(name1, name2);
-      Console.WriteLine(game.Player1.Name);
-      Console.WriteLine(game.Player2.Name);
+        while (!GameOver)
+        {
+          Console.WriteLine("Enter player 1's sign:");
+          string input1 = Console.ReadLine().ToLower();
+          Console.WriteLine("Enter player 2's sign:");
+          string input2 = Console.ReadLine().ToLower();
+          NewGame.AssignSigns(input1, input2);
 
-      Console.WriteLine("Enter player 1's sign:");
-      string input1 = Console.ReadLine().ToLower();
-      Console.WriteLine("Enter player 2's sign:");
-      string input2 = Console.ReadLine().ToLower();
-      game.AssignSigns(input1, input2);
-
-      Console.WriteLine(game.Player1.Sign);
-      Console.WriteLine(game.Player2.Sign);
-
-      
-      int result = game.CheckResult();
-      AnnounceWinner(result);
+          int result = NewGame.CheckResult();
+          AnnounceWinner(result);
+          CheckGameOver();
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("HelpLink = {0}", ex.HelpLink);
+        Console.WriteLine("Message = {0}", ex.Message);
+        Console.WriteLine("Source = {0}", ex.Source);
+        Console.WriteLine("StackTrace = {0}", ex.StackTrace);
+        Console.WriteLine("TargetSite = {0}", ex.TargetSite);
+      }
     }
 
     public static void AnnounceWinner(int result)
     {
       if (result == 1)
       {
-        Console.WriteLine("Player 1 wins");
+        Console.WriteLine($"{NewGame.Player1.Sign} vs {NewGame.Player2.Sign} = {NewGame.Player1.Sign} so {NewGame.Player1.Name} wins!");
       }
       else if (result == -1)
       {
-        Console.WriteLine("Player 2 wins");
+        Console.WriteLine($"{NewGame.Player1.Sign} vs {NewGame.Player2.Sign} = {NewGame.Player2.Sign} so {NewGame.Player2.Name} wins!");
       }
       else if (result == 0)
       {
-        Console.WriteLine("It's a draw!");
+        Console.WriteLine($"{NewGame.Player1.Sign} vs {NewGame.Player2.Sign} = It's a draw!");
       }
       else
       {
         Console.WriteLine("Invalid Input. Please enter again.");
       }
     }
+
+    public static void CheckGameOver()
+    {
+      Rounds++;
+      if (Rounds > 3)
+      { 
+        GameOver = true;
+        Console.WriteLine($"{NewGame.Player1.Name}'s score = {NewGame.Player1.Score}");
+        Console.WriteLine($"{NewGame.Player2.Name}'s score = {NewGame.Player2.Score}");
+        if (NewGame.Player1.Score > NewGame.Player2.Score)
+        {
+          Console.WriteLine($"{NewGame.Player1.Name} finally wins!");
+        }
+        else if (NewGame.Player1.Score < NewGame.Player2.Score)
+        {
+          Console.WriteLine($"{NewGame.Player2.Name} finally wins!");
+        }
+        else if (NewGame.Player1.Score == NewGame.Player2.Score)
+        {
+          Console.WriteLine($"{NewGame.Player1.Name} and {NewGame.Player2.Name} are equal!");
+        }
+        else
+        {
+          Console.WriteLine("Invalid Result");
+        }
+
+      }
+      else
+      {
+        GameOver = false;
+      }
+    }
   }
 }
+
